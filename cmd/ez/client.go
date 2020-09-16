a52c22cfd125c4dcb2342a922c664a68e2332856
aburdulescu <andrei.burdulescu@pm.me>
2020-09-16 14:42:41 +0300

    cmd/ez: Add ezs client

---
+++ cmd/ez/client.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/aburdulescu/go-ez/ezs"
+	"github.com/aburdulescu/go-ez/hash"
+	"google.golang.org/protobuf/proto"
+)
+
+type Client struct {
+	conn net.Conn
+}
+
+func (c *Client) Dial(addr string) error {
+	conn, err := net.Dial("tcp", ":8081")
+	if err != nil {
+		return err
+	}
+	c.conn = conn
+	return nil
+}
+
+func (c Client) Close() {
+	c.conn.Close()
+}
+
+func (c Client) Connect(id string) error {
+	req := &ezs.Request{
+		Type:    ezs.RequestType_CONNECT,
+		Payload: &ezs.Request_Id{id},
+	}
+	rsp, err := c.send(req)
+	if err != nil {
+		return err
+	}
+	rspType := rsp.GetType()
+	if rspType != ezs.ResponseType_ACK {
+		return fmt.Errorf("unexpected response: %s", rspType)
+	}
+	return nil
+}
+
+type GetchunkPart struct {
+	piece []byte
+	err   error
+}
+
+func (c Client) Getchunk(index uint64) (hash.Hash, chan GetchunkPart, error) {
+	req := &ezs.Request{
+		Type:    ezs.RequestType_GETCHUNK,
+		Payload: &ezs.Request_Index{index},
+	}
+	rsp, err := c.send(req)
+	if err != nil {
+		return nil, nil, err
+	}
+	rspType := rsp.GetType()
+	if rspType != ezs.ResponseType_CHUNKHASH {
+		return nil, nil, fmt.Errorf("unexpected response: %s", rspType)
+	}
+	ch := make(chan GetchunkPart)
+	go c.handleGetchunk(ch)
+	return hash.Hash(rsp.GetHash()), ch, nil
+}
+
+func (c Client) handleGetchunk(ch chan GetchunkPart) {
+	exit := false
+	b := make([]byte, 10240)
+	i := 0
+	for !exit {
+		rsp, err := c.recv(b)
+		if err != nil {
+			ch <- GetchunkPart{nil, err}
+			exit = true
+		}
+		rspType := rsp.GetType()
+		switch rspType {
+		case ezs.ResponseType_CHUNKEND:
+			exit = true
+		case ezs.ResponseType_PIECE:
+			ch <- GetchunkPart{rsp.GetPiece(), nil}
+		default:
+			ch <- GetchunkPart{nil, fmt.Errorf("unexpected response %v", rspType)}
+			exit = true
+		}
+		log.Println(i)
+		i++
+	}
+	close(ch)
+}
+
+func (c Client) send(req *ezs.Request) (*ezs.Response, error) {
+	writeBuf, err := proto.Marshal(req)
+	if err != nil {
+		return nil, err
+	}
+	if _, err := c.conn.Write(writeBuf); err != nil {
+		return nil, err
+	}
+	readBuf := make([]byte, 8192)
+	n, err := c.conn.Read(readBuf)
+	if err != nil {
+		return nil, err
+	}
+	rsp := &ezs.Response{}
+	if err := proto.Unmarshal(readBuf[:n], rsp); err != nil {
+		return nil, err
+	}
+	return rsp, nil
+}
+
+func (c Client) recv(b []byte) (*ezs.Response, error) {
+	n, err := c.conn.Read(b)
+	if err != nil {
+		return nil, err
+	}
+	rsp := &ezs.Response{}
+	if err := proto.Unmarshal(b[:n], rsp); err != nil {
+		return nil, err
+	}
+	return rsp, nil
+}
--- cmd/ez/main.go
+++ cmd/ez/main.go
@@ -5,18 +5,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"math"
-	"net"
 	"net/http"
 	"os"
 
 	"github.com/aburdulescu/go-ez/chunks"
 	"github.com/aburdulescu/go-ez/cli"
-	"github.com/aburdulescu/go-ez/ezs"
 	"github.com/aburdulescu/go-ez/ezt"
-	"github.com/aburdulescu/go-ez/hash"
-	"google.golang.org/protobuf/proto"
 )
 
 var c = cli.New(os.Args[0], []cli.Cmd{
@@ -108,56 +103,28 @@ func onGet(args ...string) error {
 }
 
 func fetch(name string, id string) error {
-	conn, err := net.Dial("tcp", ":8081")
+	var client Client
+	if err := client.Dial(":8081"); err != nil {
+		return err
+	}
+	defer client.Close()
+	if err := client.Connect(id); err != nil {
+		return err
+	}
+	chunkHash, ch, err := client.Getchunk(0)
 	if err != nil {
 		return err
 	}
-	defer conn.Close()
-	req := &ezs.Request{
-		Type:    ezs.RequestType_CONNECT,
-		Payload: &ezs.Request_Id{id},
-	}
-	_, err = sendReq(conn, req)
-	if err != nil {
-		return err
-	}
-	req = &ezs.Request{
-		Type:    ezs.RequestType_GETCHUNK,
-		Payload: &ezs.Request_Index{0},
-	}
-	rsp, err := sendReq(conn, req)
-	if err != nil {
-		return err
-	}
-	chunkHash := hash.Hash(rsp.GetHash())
-	fmt.Printf("%v, %s\n", rsp.GetType(), chunkHash)
-	exit := false
-	b := make([]byte, 10240)
+	fmt.Printf("%s\n", chunkHash)
 	buf := new(bytes.Buffer)
-	i := 0
-	for !exit {
-		n, err := conn.Read(b)
+	for part := range ch {
+		if part.err != nil {
+			return err
+		}
+		_, err := buf.Write(part.piece)
 		if err != nil {
 			return err
 		}
-		log.Println(n)
-		rsp := &ezs.Response{}
-		if err := proto.Unmarshal(b[:n], rsp); err != nil {
-			return err
-		}
-		rspType := rsp.GetType()
-		switch rspType {
-		case ezs.ResponseType_CHUNKEND:
-			exit = true
-		case ezs.ResponseType_PIECE:
-			_, err := buf.Write(b[:n])
-			if err != nil {
-				return err
-			}
-		default:
-		}
-		log.Println(i)
-		i++
 	}
 	f, err := os.Create(name)
 	if err != nil {
@@ -168,24 +135,4 @@ func fetch(name string, id string) error {
 		return err
 	}
 	return nil
-}
-
-func sendReq(conn net.Conn, req *ezs.Request) (*ezs.Response, error) {
-	writeBuf, err := proto.Marshal(req)
-	if err != nil {
-		return nil, err
-	}
-	if _, err := conn.Write(writeBuf); err != nil {
-		return nil, err
-	}
-	readBuf := make([]byte, 8192)
-	n, err := conn.Read(readBuf)
-	if err != nil {
-		return nil, err
-	}
-	rsp := &ezs.Response{}
-	if err := proto.Unmarshal(readBuf[:n], rsp); err != nil {
-		return nil, err
-	}
-	return rsp, nil
 }
