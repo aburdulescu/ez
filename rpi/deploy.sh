a08115311c2a34ca043d05e53cad94fc0638838e
aburdulescu <andrei.burdulescu@pm.me>
2020-10-03 17:53:35 +0300

    Add templates for config files

--- rpi/deploy.sh
+++ rpi/deploy.sh
@@ -14,3 +14,35 @@
 # copy this folder to the target
 # generate the files there? no, because then the seeders will have different files
 # then generate them in one place and copy(maybe using ez!) them where needed and if needed
+
+cfg=$(cat config.json)
+homePath=$(echo $cfg | jq -r ".homePath")
+trackerAddr=$(echo $cfg | jq -r ".trackerAddr")
+seeders=$(echo $cfg | jq -r ".seeders")
+
+seedersLength=$(echo $seeders | jq "length")
+
+echo $homePath
+echo $trackerAddr
+echo $seedersLength
+
+ezDataFmt='{"trackerAddr":"%s"}'
+ezlDataFmt='{"trackerAddr":"%s","seederAddr":"%s","dbPath":"./db"}'
+ezsDataFmt='{"dbPath":"./db"}'
+
+for i in $(seq 0 $(($seedersLength-1)))
+do
+    seeder=$(echo $seeders | jq -r ".[$i]")
+    addr=$(echo $seeder | jq -r ".addr")
+    isTracker=$(echo $seeder | jq -r ".isTracker")
+    isClient=$(echo $seeder | jq -r ".isClient")
+    echo $addr $isTracker $isClient
+
+    seederDir="seeder_"$addr
+    rm -rf $seederDir
+    mkdir -p $seederDir
+
+    printf $ezDataFmt $trackerAddr | tpl -t templates/ez.json.tpl > $seederDir/ez.json
+    printf $ezlDataFmt $trackerAddr $addr | tpl -t templates/ezl.json.tpl > $seederDir/ezl.json
+    printf $ezsDataFmt | tpl -t templates/ezs.json.tpl > $seederDir/ezs.json
+done
