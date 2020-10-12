f8b4d4d4121b4af69480fadc7585087ad6c54a35
aburdulescu <andrei.burdulescu@pm.me>
2020-10-12 15:25:30 +0300

    cmd/ez,ezl: Don't print the error twice

--- cmd/ez/main.go
+++ cmd/ez/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	// "github.com/pkg/profile"
@@ -12,7 +11,6 @@ func main() {
 	// defer profile.Start(profile.ProfilePath("."), profile.MemProfile, profile.MemProfileRate(1)).Stop()
 	log.SetFlags(log.Lshortfile | log.Ltime | log.Lmicroseconds | log.LUTC)
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 }
--- cmd/ezl/main.go
+++ cmd/ezl/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"os"
 )
 
 func main() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 }
