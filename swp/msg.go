6cfcc8022b41ca0753dfea0e22450b3b8386a7a9
aburdulescu <andrei.burdulescu@pm.me>
2020-10-29 18:45:28 +0200

    cmd/swp: Add message definitions

---
+++ swp/msg.go
@@ -0,0 +1,63 @@
+package swp
+
+type MsgType uint8
+
+const (
+	CONNECT MsgType = iota
+	DISCONNECT
+	GETCHUNK
+	ACK
+	CHUNKHASH
+	PIECE
+)
+
+type Msg interface {
+	Type() MsgType
+}
+
+type Connect struct {
+	Id string
+}
+
+func (r Connect) Type() MsgType {
+	return CONNECT
+}
+
+type Disonnect struct {
+}
+
+func (r Disonnect) Type() MsgType {
+	return DISCONNECT
+}
+
+type Getchunk struct {
+	Index uint64
+}
+
+func (r Getchunk) Type() MsgType {
+	return GETCHUNK
+}
+
+type Ack struct {
+}
+
+func (r Ack) Type() MsgType {
+	return ACK
+}
+
+type Chunkhash struct {
+	NPieces uint64
+	Hash    []byte
+}
+
+func (r Chunkhash) Type() MsgType {
+	return CHUNKHASH
+}
+
+type Piece struct {
+	Piece []byte
+}
+
+func (r Piece) Type() MsgType {
+	return PIECE
+}
