ba41b3beb226356ae6cd2d7e91b9741e4e1bef47
aburdulescu <andrei.burdulescu@pm.me>
2020-12-24 15:03:11 +0200

    cmd/ezs: Handle rm and mv file events

--- cmd/ezs/file_watcher.go
+++ cmd/ezs/file_watcher.go
@@ -10,30 +10,37 @@ import (
 type Watcher struct {
 	watcher *fsnotify.Watcher
 	db      *DB
+	fileIds map[string]string
 }
 
 func NewWatcher(db *DB) (*Watcher, error) {
-	w, err := fsnotify.NewWatcher()
+	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		return nil, err
 	}
+	w := &Watcher{
+		watcher: watcher,
+		db:      db,
+		fileIds: make(map[string]string),
+	}
 	files, err := db.GetAll()
 	if err != nil {
-		return err
+		return nil, err
 	}
 	for _, file := range files {
-		path := filepath.Join(file.ifile.Dir, file.ifile.Name)
-		if err := w.Add(path); err != nil {
-			return err
+		path := filepath.Join(file.IFile.Dir, file.IFile.Name)
+		if err := w.Add(path, file.Id); err != nil {
+			return nil, err
 		}
 	}
-	return &Watcher{w, db}, nil
+	return w, nil
 }
 
-func (w Watcher) Add(path string) error {
+func (w Watcher) Add(path, id string) error {
 	if err := w.watcher.Add(path); err != nil {
 		return err
 	}
+	w.fileIds[path] = id
 	return nil
 }
 
@@ -47,11 +54,18 @@ func (w Watcher) Run() {
 			log.Println("event:", event)
 			switch {
 			case (event.Op & (fsnotify.Remove | fsnotify.Rename)) != 0:
-				log.Println("remove file from db")
+				id, ok := w.fileIds[event.Name]
+				if !ok {
+					log.Printf("%s not found", event.Name)
+					continue
+				}
+				if err := w.db.Delete(id); err != nil {
+					log.Println(err)
+					continue
+				}
 			case (event.Op & fsnotify.Write) != 0:
 				log.Println("remove file, process it and add it to db")
 			default:
-				log.Println("do nothing")
 			}
 		case err, ok := <-w.watcher.Errors:
 			if !ok {
